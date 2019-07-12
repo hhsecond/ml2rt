aa65a788190d8c4bf85ffaa3f8bb23904d7c6f0b
hhsecond <sherin@tensorwerk.com>
2019-07-12 13:46:02 +0530

    tests

--- tests/test_mlutil.py
+++ tests/test_mlutil.py
@@ -1,13 +1,11 @@
 import time
 import os
 import sys
-from unittest import TestCase
 
-from redisai import save_model, load_model
-from redisai import (
+from mlutil import load_model, load_script
+from mlutil import (
     save_tensorflow, save_torch, save_onnx, save_sklearn, save_sparkml)
-from redisai.model import onnx_utils
-from redisai import Client, Backend, Device, Tensor, DType, BlobTensor
+from mlutil import utils
 import tensorflow as tf
 import torch
 from sklearn import linear_model, datasets
@@ -44,10 +42,6 @@ def get_sklearn_model_and_prototype():
     return model, X[0].reshape(1, -1).astype(np.float32)
 
 
-def get_dummy_prototype():
-    return np.array([1, 2])
-
-
 def get_onnx_model():
     torch_model = torch.nn.ReLU()
     # maybe there exists, but couldn't find a way to pass
@@ -79,183 +73,95 @@ def get_spark_model_and_prototype():
     return model, prototype
 
 
-class ModelTestCase(TestCase):
+class TestModel:
+    # TODO: Detailed tests
 
-    def get_client(self):
-        return Client()
-
-    def testTFGraph(self):
-        _ = get_tf_graph()
+    def test_TFGraph(self):
+        get_tf_graph()
         init = tf.global_variables_initializer()
         sess = tf.Session()
         sess.run(init)
         path = f'{time.time()}.pb'
         save_tensorflow(sess, path, output=['output'])
-        model = load_model(path)
+        assert os.path.exists(path)
         os.remove(path)
-        con = self.get_client()
-        con.modelset(
-            'tfmodel', Backend.tf, Device.cpu, model,
-            input=['input'], output=['output'])
-        con.tensorset('a', Tensor.scalar(DType.float, 2))
-        con.modelrun('tfmodel', ['a'], 'c')
-        tensor = con.tensorget('c')
-        self.assertEqual([13], tensor.value)
 
-    def testPyTorchGraph(self):
+    def test_PyTorchGraph(self):
         torch_graph = MyModule()
         path = f'{time.time()}.pt'
         save_torch(torch_graph, path)
-        model = load_model(path)
+        assert os.path.exists(path)
         os.remove(path)
-        con = self.get_client()
-        con.modelset('ptmodel', Backend.torch, Device.cpu, model)
-        con.tensorset('a', Tensor.scalar(DType.float, 2, 5))
-        con.tensorset('b', Tensor.scalar(DType.float, 3, 7))
-        con.modelrun('ptmodel', ['a', 'b'], 'c')
-        tensor = con.tensorget('c')
-        self.assertEqual([5, 12], tensor.value)
 
-    def testFakeObjSave(self):
-        fakemodel = {}
-        prototype = get_dummy_prototype()
-        self.assertRaises(
-            AttributeError,
-            save_torch, fakemodel, 'fake.pt')
-        wrongmodel_pt = torch.nn.Linear(2, 3)
-        self.assertRaises(
-            AttributeError,
-            save_torch, wrongmodel_pt, 'wrong.pt')
-        self.assertRaises(
-            AttributeError,
-            save_tensorflow, wrongmodel_pt, 'wrong.pt', output=['output'])
-        self.assertRaises(
-            AttributeError,
-            save_onnx, wrongmodel_pt, 'wrong.pt')
-        self.assertRaises(
-            RuntimeError,
-            save_sklearn, wrongmodel_pt, 'wrong.pt', prototype=prototype)
-        if os.path.isfile('wrong.pt'):
-            os.remove('wrong.pt')
-
-    def testSaveModelDeprecation(self):
-        torch_graph = MyModule()
-        self.assertRaises(
-            DeprecationWarning,
-            save_model, torch_graph, 'wrong.pt')
-
-    def testScriptLoad(self):
-        con = self.get_client()
+    def test_ScriptLoad(self):
         dirname = os.path.dirname(__file__)
         path = f'{dirname}/testdata/script.txt'
-        script = load_model(path)
-        con.scriptset('script', Device.cpu, script)
-        con.tensorset('a', Tensor.scalar(DType.float, 2, 5))
-        con.tensorset('b', Tensor.scalar(DType.float, 3, 7))
-        con.scriptrun('script', 'bar', ['a', 'b'], 'c')
-        tensor = con.tensorget('c')
-        self.assertEqual([5, 12], tensor.value)
+        load_script(path)
 
-    def testONNXGraph(self):
+    def test_ONNXGraph(self):
         onnx_model = get_onnx_model()
         path = f'{time.time()}.onnx'
         save_onnx(onnx_model, path)
-        model = load_model(path)
+        assert os.path.exists(path)
+        load_model(path)
         os.remove(path)
-        con = self.get_client()
-        con.modelset('onnxmodel', Backend.onnx, Device.cpu, model)
-        con.tensorset('a', Tensor.scalar(DType.float, 2, -1))
-        con.modelrun('onnxmodel', ['a'], ['c'])
-        tensor = con.tensorget('c')
-        self.assertEqual([2.0, 0.0], tensor.value)
 
-    def testSKLearnGraph(self):
+    def test_SKLearnGraph(self):
         sklearn_model, prototype = get_sklearn_model_and_prototype()
-        path = f'{time.time()}.onnx'
-        self.assertRaises(RuntimeError, save_sklearn, sklearn_model, path)
-        con = self.get_client()
 
         # saving with prototype
         path = f'{time.time()}.onnx'
         save_sklearn(sklearn_model, path, prototype=prototype)
-        model = load_model(path)
+        assert os.path.exists(path)
+        load_model(path)
         os.remove(path)
-        con.modelset('onnx_skl_model1', Backend.onnx, Device.cpu, model)
-        con.tensorset('a1', Tensor.scalar(DType.float, *([1] * 13)))
-        con.modelrun('onnx_skl_model1', ['a1'], ['outfromonnxskl1'])
-        tensor = con.tensorget('outfromonnxskl1')
-        self.assertEqual(len(tensor.value), 1)
 
         # saving with shape and dtype
         shape = prototype.shape
         if prototype.dtype == np.float32:
-            dtype = DType.float32
+            dtype = prototype.dtype
         else:
             raise RuntimeError("Test is not configured to run with another type")
         path = f'{time.time()}.onnx'
         save_sklearn(sklearn_model, path, shape=shape, dtype=dtype)
-        model = load_model(path)
+        assert os.path.exists(path)
+        load_model(path)
         os.remove(path)
-        con.modelset('onnx_skl_model2', Backend.onnx, Device.cpu, model)
-        con.tensorset('a2', Tensor.scalar(DType.float, *([1] * 13)))
-        con.modelrun('onnx_skl_model2', ['a2'], ['outfromonnxskl2'])
-        tensor = con.tensorget('outfromonnxskl2')
-        self.assertEqual(len(tensor.value), 1)
 
         # saving with initial_types
-        inital_types = onnx_utils.get_tensortype(shape, dtype)
+        inital_types = utils.guess_onnx_tensortype(shape=shape, dtype=dtype)
         path = f'{time.time()}.onnx'
         save_sklearn(sklearn_model, path, initial_types=[inital_types])
-        model = load_model(path)
+        assert os.path.exists(path)
+        load_model(path)
         os.remove(path)
-        con.modelset('onnx_skl_model3', Backend.onnx, Device.cpu, model)
-        con.tensorset('a3', Tensor.scalar(DType.float, *([1] * 13)))
-        con.modelrun('onnx_skl_model3', ['a3'], ['outfromonnxskl3'])
-        tensor = con.tensorget('outfromonnxskl3')
-        self.assertEqual(len(tensor.value), 1)
 
-    def testSparkMLGraph(self):
+    def test_SparkMLGraph(self):
         spark_model, prototype = get_spark_model_and_prototype()
-        in_tensor = BlobTensor.from_numpy(prototype)
-        path = f'{time.time()}.onnx'
-        self.assertRaises(RuntimeError, save_sparkml, spark_model, path)
-        con = self.get_client()
 
         # saving with prototype
         path = f'{time.time()}.onnx'
         save_sparkml(spark_model, path, prototype=prototype)
-        model = load_model(path)
+        load_model(path)
+        assert os.path.exists(path)
         os.remove(path)
-        con.modelset('spark_model1', Backend.onnx, Device.cpu, model)
-        con.tensorset('a1', in_tensor)
-        con.modelrun('spark_model1', ['a1'], ['outfromspark1'])
-        tensor = con.tensorget('outfromspark1')
-        self.assertEqual(len(tensor.value), 1)
 
         # saving with shape and dtype
         shape = prototype.shape
         if prototype.dtype == np.float32:
-            dtype = DType.float32
+            dtype = prototype.dtype
         else:
             raise RuntimeError("Test is not configured to run with another type")
         path = f'{time.time()}.onnx'
         save_sparkml(spark_model, path, shape=shape, dtype=dtype)
-        model = load_model(path)
+        assert os.path.exists(path)
+        load_model(path)
         os.remove(path)
-        con.modelset('spark_model2', Backend.onnx, Device.cpu, model)
-        con.tensorset('a2', in_tensor)
-        con.modelrun('spark_model2', ['a2'], ['outfromspark2'])
-        tensor = con.tensorget('outfromspark2')
-        self.assertEqual(len(tensor.value), 1)
 
         # saving with initial_types
-        inital_types = onnx_utils.get_tensortype(shape, dtype)
+        inital_types = utils.guess_onnx_tensortype(shape=shape, dtype=dtype)
         path = f'{time.time()}.onnx'
         save_sparkml(spark_model, path, initial_types=[inital_types])
-        model = load_model(path)
+        assert os.path.exists(path)
+        load_model(path)
         os.remove(path)
-        con.modelset('spark_model3', Backend.onnx, Device.cpu, model)
-        con.tensorset('a3', in_tensor)
-        con.modelrun('spark_model3', ['a3'], ['outfromspark3'])
-        tensor = con.tensorget('outfromspark3')
-        self.assertEqual(len(tensor.value), 1)
